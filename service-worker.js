73c83bdc128a7c2b91208cf228406fe6f95c3dc0
Rupert1982 <rupert.j.31@gmx.de>
2026-07-27 21:07:25 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "luca6-v1";
+const CACHE = "luca6-v2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
